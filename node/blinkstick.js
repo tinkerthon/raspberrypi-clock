9c68cc913374aca5f7b2e5db11c8e7a3c0361f77
Olav Schettler <olav@schettler.net>
2014-12-07 11:25:04 +0100

    Blinkstick Controller

---
+++ node/blinkstick.js
@@ -0,0 +1,39 @@
+/*
+ */
+var blinkstick = require('blinkstick');
+
+exports.index = function(req, res) {
+  var led = blinkstick.findFirst();
+
+  if (typeof req.query.op === 'undefined') {
+    res.send({ status: 'ERR' });
+  }
+  else
+  if (req.query.op == 'random') {
+    led.setColor('random', function () {
+      res.send({ status: 'OK' });
+    });
+  }
+  else
+  if (req.query.op == 'off') {
+    led.turnOff();
+    res.send({ status: 'OK' });
+  }
+  else
+  if (req.query.op == 'pulse') {
+    led.pulse(req.query.rgb, function () {
+      res.send({ status: 'OK' });
+    });
+  }
+  else
+  if (req.query.op == 'blink') {
+    led.blink(req.query.rgb, function () {
+      res.send({ status: 'OK' });
+    });
+  }
+  else {
+    led.setColor(req.query.op, function () {
+      res.send({ status: 'OK' });
+    });
+  }
+};
