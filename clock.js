568175bd68e9d75f3f52f2d95af5f9e7a59c6be7
Olav Schettler <olav@schettler.net>
2014-12-07 19:08:40 +0100

    Modified clock.js

---
+++ clock.js
@@ -0,0 +1,39 @@
+/**
+ * clock.js
+ */
+jQuery(function () {
+    // every 1s
+    setInterval(function () {
+        var d = new Date,
+            hour = d.getHours(),
+            min = d.getMinutes();
+
+        $('#time').text(
+            (hour < 10 ? '0' : '') + String(hour)
+            + ':' + (min < 10 ? '0' : '') + String(min)
+            + ' Uhr'
+        );
+    }, 1000);
+    
+    // every 5m
+    setInterval(function () {
+        $('#message').load('https://decoupled.de/lena.php');
+    }, 300000);
+    
+    $('#menu').on('click', 'a', function (e) {
+        var href = $(this).attr('href');
+        
+        if (href.match(/^javascript:/)) {
+            return;
+        }
+
+        $('#menu a').removeClass('active');
+        $(this).addClass('active');
+
+        $('section').addClass('hidden');
+        $(href).removeClass('hidden');
+
+        e.preventDefault();
+    });
+});
+
