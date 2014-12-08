28f8dfdb35fe157669c94b800616432c8c893435
Olav Schettler <olav@schettler.net>
2014-12-08 23:07:40 +0100

    Clock with Blinkstick lamp, ExpressJS style

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
+var blinkstick = require('blinkstick');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+function bright(res, led, rgb) {
+  led.morph(rgb, {index: 0}, function () {
+    led.morph(rgb, {index: 2}, function () {
+      led.morph(rgb, {index: 4}, function () {
+        led.morph(rgb, {index: 6}, function () {
+          res.send({ status: 'OK' });    
+        });
+      });
+    });
+  });
+}
+
+router.get('/blinkstick', function(req, res) {
+  var led = blinkstick.findFirst();
+
+  if (typeof req.query.op === 'undefined') {
+    res.send({ status: 'ERR' });
+  }
+  else
+  if (req.query.op == 'random') {
+    led.setColor('random', function () {
+      res.send({ status: 'OK' });
+    });
+  }
+  else
+  if (req.query.op == 'off') {
+    led.turnOff();
+    res.send({ status: 'OK' });
+  }
+  else
+  if (req.query.op == 'pulse') {
+    led.pulse(req.query.rgb, function () {
+      res.send({ status: 'OK' });
+    });
+  }
+  else
+  if (req.query.op == 'morph') {
+    bright(res, led, req.query.rgb);
+  }
+  else {
+    led.setColor(req.query.op, function () {
+      res.send({ status: 'OK' });
+    });
+  }
 });
 
 module.exports = router;
